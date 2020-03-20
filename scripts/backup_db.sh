be14f2c431123d81cb9afb32cf989a3e6b233236
Daniel Boline <ddboline@gmail.com>
2020-03-19 23:41:59 -0400

    bugfix

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -13,5 +13,5 @@ mkdir -p backup
 for T in $TABLES;
 do
     psql $DB -c "COPY $T TO STDOUT" | gzip > backup/${T}.sql.gz
-    calendar s3 cp backup/${T}.sql.gz s3://${BUCKET}/${T}.sql.gz
+    aws s3 cp backup/${T}.sql.gz s3://${BUCKET}/${T}.sql.gz
 done
--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -12,6 +12,6 @@ mkdir -p backup/
 
 for T in $TABLES;
 do
-    calendar s3 cp s3://${BUCKET}/${T}.sql.gz backup/${T}.sql.gz
+    aws s3 cp s3://${BUCKET}/${T}.sql.gz backup/${T}.sql.gz
     gzip -dc backup/${T}.sql.gz | psql $DB -c "COPY $T FROM STDIN"
 done
