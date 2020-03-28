c59b171f6d79be6c02e39744c40f55e8980dcc58
Daniel Boline <ddboline@gmail.com>
2020-03-28 17:06:38 -0400

    update sequence when restoring db

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -15,3 +15,6 @@ do
     aws s3 cp s3://${BUCKET}/${T}.sql.gz backup/${T}.sql.gz
     gzip -dc backup/${T}.sql.gz | psql $DB -c "COPY $T FROM STDIN"
 done
+
+psql $DB -c "select setval('calendar_list_id_seq', (select max(id) from calendar_list), TRUE)"
+psql $DB -c "select setval('calendar_cache_id_seq', (select max(id) from calendar_cache), TRUE)"
