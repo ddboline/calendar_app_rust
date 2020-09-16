2ac761734d27bd65df69449e649c0e426691b41d
Daniel Boline <ddboline@gmail.com>
2020-09-16 09:44:17 -0400

    Use auth server rust (#4)
    * rust-auth-server -> auth_server_rust

--- calendar_app_http/src/app.rs
+++ calendar_app_http/src/app.rs
@@ -1,12 +1,14 @@
 use actix_identity::{CookieIdentityPolicy, IdentityService};
 use actix_web::{web, App, HttpServer};
+use anyhow::Error;
+use lazy_static::lazy_static;
 use std::time::Duration;
 use tokio::time::interval;
 
 use calendar_app_lib::{calendar_sync::CalendarSync, config::Config, pgpool::PgPool};
 
 use crate::{
-    logged_user::{fill_from_db, TRIGGER_DB_UPDATE},
+    logged_user::{fill_from_db, get_secrets, SECRET_KEY, TRIGGER_DB_UPDATE},
     routes::{
         agenda, build_calendar_event, calendar_cache, calendar_cache_update, calendar_index,
         calendar_list, calendar_list_update, create_calendar_event, delete_event, edit_calendar,
@@ -15,22 +17,26 @@ use crate::{
     },
 };
 
+lazy_static! {
+    pub static ref CONFIG: Config = Config::init_config().expect("Failed to load config");
+}
+
 pub struct AppState {
     pub cal_sync: CalendarSync,
 }
 
-pub async fn start_app() {
+pub async fn start_app() -> Result<(), Error> {
     async fn _update_db(pool: PgPool) {
         let mut i = interval(Duration::from_secs(60));
         loop {
-            i.tick().await;
             fill_from_db(&pool).await.unwrap_or(());
+            i.tick().await;
         }
     }
     TRIGGER_DB_UPDATE.set();
-    let config = Config::init_config().expect("Failed to load config");
-    let pool = PgPool::new(&config.database_url);
-    let cal_sync = CalendarSync::new(config, pool);
+    get_secrets(&CONFIG.secret_path, &CONFIG.jwt_secret_path).await?;
+    let pool = PgPool::new(&CONFIG.database_url);
+    let cal_sync = CalendarSync::new(CONFIG.clone(), pool);
 
     actix_rt::spawn(_update_db(cal_sync.pool.clone()));
     let port = cal_sync.config.port;
@@ -41,7 +47,7 @@ pub async fn start_app() {
                 cal_sync: cal_sync.clone(),
             })
             .wrap(IdentityService::new(
-                CookieIdentityPolicy::new(cal_sync.config.secret_key.as_bytes())
+                CookieIdentityPolicy::new(&SECRET_KEY.load())
                     .name("auth")
                     .path("/")
                     .domain(cal_sync.config.domain.as_str())
@@ -85,5 +91,5 @@ pub async fn start_app() {
     .unwrap_or_else(|_| panic!("Failed to bind to port {}", port))
     .run()
     .await
-    .expect("Failed to start app");
+    .map_err(Into::into)
 }
--- src/calendar_app_http.rs
+++ src/calendar_app_http.rs
@@ -3,5 +3,5 @@ use calendar_app_http::app::start_app;
 #[actix_rt::main]
 async fn main() {
     env_logger::init();
-    start_app().await;
+    start_app().await.unwrap();
 }
