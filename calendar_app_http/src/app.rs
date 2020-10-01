cd8ef1a7e3bdab59524dec76984cf65bf3c622fb
Daniel Boline <ddboline@gmail.com>
2020-10-01 15:24:57 -0400

    compress middleware

--- calendar_app_http/src/app.rs
+++ calendar_app_http/src/app.rs
@@ -1,5 +1,5 @@
 use actix_identity::{CookieIdentityPolicy, IdentityService};
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware::Compress};
 use anyhow::Error;
 use lazy_static::lazy_static;
 use std::time::Duration;
@@ -46,6 +46,7 @@ pub async fn start_app() -> Result<(), Error> {
             .data(AppState {
                 cal_sync: cal_sync.clone(),
             })
+            .wrap(Compress::default())
             .wrap(IdentityService::new(
                 CookieIdentityPolicy::new(&SECRET_KEY.get())
                     .name("auth")
