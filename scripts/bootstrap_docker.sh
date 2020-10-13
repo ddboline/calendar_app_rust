4a5b967bf6f4e958654e04f28362b524b1a59fb8
Daniel Boline <ddboline@gmail.com>
2020-10-13 18:35:11 -0400

    ...

--- scripts/bootstrap_docker.sh
+++ scripts/bootstrap_docker.sh
@@ -5,13 +5,13 @@ if [ -z "$PASSWORD" ]; then
 fi
 DB=calendar_app_cache
 
-docker run --name $DB -p 12345:5432 -e POSTGRES_PASSWORD=$PASSWORD -d postgres
+docker run --name $DB -p 5432:5432 -e POSTGRES_PASSWORD=$PASSWORD -d postgres
 sleep 10
-DATABASE_URL="postgresql://postgres:$PASSWORD@localhost:12345/postgres"
+DATABASE_URL="postgresql://postgres:$PASSWORD@localhost:5432/postgres"
 
 psql $DATABASE_URL -c "CREATE DATABASE $DB"
 
-DATABASE_URL="postgresql://postgres:$PASSWORD@localhost:12345/$DB"
+DATABASE_URL="postgresql://postgres:$PASSWORD@localhost:5432/$DB"
 
 mkdir -p ${HOME}/.config/calendar_app_rust
 cat > ${HOME}/.config/calendar_app_rust/config.env <<EOL
