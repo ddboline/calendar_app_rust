2258410b05be20e68f58668b9be503ed27aeefbf
Daniel Boline <ddboline@gmail.com>
2021-02-25 20:23:20 -0500

    use deadqueue

--- gcal_lib/src/rate_limiter.rs
+++ gcal_lib/src/rate_limiter.rs
@@ -1,73 +1,61 @@
 use anyhow::Error;
 use chrono::Utc;
-use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
+use deadqueue::limited::Queue;
+use log::debug;
+use std::sync::Arc;
 use tokio::{
     sync::Mutex,
-    task::JoinHandle,
+    task::{spawn, JoinHandle},
     time::{sleep, Duration},
 };
 
+#[derive(Clone)]
 pub struct RateLimiter {
-    max_per_unit_time: usize,
-    unit_time_ms: usize,
-    until: AtomicUsize,
-    current_count: AtomicIsize,
-    limit_mutex: Mutex<()>,
+    inner: Arc<RateLimiterInner>,
+    #[allow(dead_code)]
+    rate_task: Arc<JoinHandle<()>>,
 }
 
 impl RateLimiter {
     pub fn new(max_per_unit_time: usize, unit_time_ms: usize) -> Self {
-        Self {
-            max_per_unit_time,
-            unit_time_ms,
-            until: AtomicUsize::new(Utc::now().timestamp_millis() as usize),
-            current_count: AtomicIsize::new(max_per_unit_time as isize),
-            limit_mutex: Mutex::new(()),
-        }
-    }
-
-    fn check_reset(&self) {
-        let current = Utc::now().timestamp_millis() as usize;
-        let until = self.until.load(Ordering::SeqCst);
-        if current > until {
-            let new = current + self.unit_time_ms;
-            if self
-                .until
-                .compare_exchange(until, new, Ordering::SeqCst, Ordering::SeqCst)
-                .is_ok()
-            {
-                self.current_count
-                    .store(self.max_per_unit_time as isize, Ordering::SeqCst);
-            }
-        }
+        let inner = Arc::new(RateLimiterInner::new(max_per_unit_time, unit_time_ms));
+        let rate_task = Arc::new({
+            let inner = inner.clone();
+            spawn(async move {
+                inner.check_reset().await;
+            })
+        });
+        Self { inner, rate_task }
     }
 
     pub async fn acquire(&self) {
+        self.inner.rate_queue.pop().await;
+    }
+}
+
+struct RateLimiterInner {
+    max_per_unit_time: usize,
+    unit_time_ms: usize,
+    rate_queue: Queue<()>,
+}
+
+impl RateLimiterInner {
+    fn new(max_per_unit_time: usize, unit_time_ms: usize) -> Self {
+        Self {
+            max_per_unit_time,
+            unit_time_ms,
+            rate_queue: Queue::new(max_per_unit_time),
+        }
+    }
+
+    async fn check_reset(&self) {
         loop {
-            self.check_reset();
-
-            if self.current_count.fetch_sub(1, Ordering::SeqCst) > 0 {
-                return;
-            }
-
-            if let Ok(_lock) = self.limit_mutex.try_lock() {
-                self.current_count.fetch_add(1, Ordering::SeqCst);
-                let current = Utc::now().timestamp_millis();
-                let remaining = self.until.load(Ordering::SeqCst) as i64 - current;
-                if remaining > 0 {
-                    sleep(Duration::from_millis(remaining as u64)).await;
-                }
-                self.check_reset();
-                if self.current_count.fetch_sub(1, Ordering::SeqCst) > 0 {
-                    return;
-                }
-            } else {
-                let _lock = self.limit_mutex.lock().await;
-                self.check_reset();
-                if self.current_count.fetch_sub(1, Ordering::SeqCst) > 0 {
-                    return;
+            for _ in 0..self.max_per_unit_time {
+                if self.rate_queue.try_push(()).is_err() {
+                    break;
                 }
             }
+            sleep(Duration::from_millis(self.unit_time_ms as u64)).await;
         }
     }
 }
@@ -92,7 +80,7 @@ mod tests {
     async fn test_rate_limiter() -> Result<(), Error> {
         env_logger::init();
 
-        let rate_limiter = Arc::new(RateLimiter::new(1000, 100));
+        let rate_limiter = RateLimiter::new(1000, 100);
         let test_count = Arc::new(AtomicUsize::new(0));
 
         let start = Utc::now();
@@ -108,18 +96,13 @@ mod tests {
             })
             .collect();
 
-        sleep(Duration::from_millis(20)).await;
-
-        let count = test_count.load(Ordering::SeqCst);
-        debug!("{}", count);
-        assert!(count > 0 && count <= 1000);
-
         sleep(Duration::from_millis(100)).await;
 
-        let count = test_count.load(Ordering::SeqCst);
-        debug!("{}", count);
-        assert!(count > 1000 && count <= 2000);
-
+        for _ in 0..5 {
+            let count = test_count.load(Ordering::SeqCst);
+            debug!("{}", count);
+            sleep(Duration::from_millis(100)).await;
+        }
         for t in tasks {
             t.await?;
         }
