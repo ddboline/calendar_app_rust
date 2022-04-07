7c6f9def6f0cd91ab2d93d04d2924b797cc772ed
Daniel Boline <ddboline@gmail.com>
2022-04-06 23:19:32 -0400

    refactor

--- src/calendar_app_http.rs
+++ src/calendar_app_http.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::semicolon_if_nothing_returned)]
 
+use anyhow::Error;
+
 use calendar_app_http::app::start_app;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     env_logger::init();
-    start_app().await.unwrap();
+    start_app().await
 }
