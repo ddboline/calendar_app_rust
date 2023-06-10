9a9e37e5d57358decd26a9527f551f1a43202c99
Daniel Boline <ddboline@gmail.com>
2023-06-10 16:09:12 -0400

    clippy

--- src/calendar_app_bot.rs
+++ src/calendar_app_bot.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), Error> {
         let pool = PgPool::new(&config.database_url);
         if let Some(telegram_bot_token) = config.telegram_bot_token.as_ref() {
             let bot = TelegramBot::new(telegram_bot_token, &pool, &config).await;
-            bot.run().await?;
+            Box::pin(bot.run()).await?;
         }
         Ok(())
     })
